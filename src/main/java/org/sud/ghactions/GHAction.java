cf1f89991175168b9340e2818e0e12073b0dad8f
Sud <sudarsana.ala@gmail.com>
2023-03-24 18:43:28 +0530

    initial commit

---
+++ src/main/java/org/sud/ghactions/GHAction.java
@@ -0,0 +1,12 @@
+package org.sud.ghactions;
+
+public class GHAction {
+
+    public static void main(String args[]){
+        try {
+            System.out.println("Java Run Action Called");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
