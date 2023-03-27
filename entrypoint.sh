bb3bfbbe79bf2179b2aa1aa011b79824cc32d412
sudarsana-reddy <sudarsana.ala@gmail.com>
2023-03-27 19:34:30 +0530

    copy file to runner

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,10 @@ pwd
 echo "Building the java project"
 mvn clean install -f /app/pom.xml
 
+echo "copy jar to local runner"
+cp /app/target/java-custom-action-1.0-SNAPSHOT.jar $(pwd)
+
 echo "Running Java Program"
 java -jar /app/target/java-custom-action-1.0-SNAPSHOT.jar
-
-echo "Docker hostname"
-cat /etc/hostname
-
-echo "Container list"
-docker container ls
 
 echo "time=$(date)" >> $GITHUB_OUTPUT
