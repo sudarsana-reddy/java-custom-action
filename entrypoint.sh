ddbca51669c3b501896e7e1826e576a1f1e50969
sudarsana-reddy <sudarsana.ala@gmail.com>
2023-03-27 17:09:18 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,16 @@ echo "hello world"
 
 pwd
 
+echo "Building the java project"
 mvn clean install -f /app/pom.xml
 
+echo "Running Java Program"
 java -jar /app/target/java-custom-action-1.0-SNAPSHOT.jar
+
+echo "Docker hostname"
+cat /etc/hostname
+
+echo "Container list"
+docker container ls
 
 echo "time=$(date)" >> $GITHUB_OUTPUT
